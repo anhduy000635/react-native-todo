204eb1d4adab4fc25803079a1b48b1fdf2fa43d4
anhduy00635 <anhduy000635@gmail.com>
2025-01-20 15:40:56 +0700

    Fonts

--- components/review/detail.tsx
+++ components/review/detail.tsx
@@ -1,10 +1,11 @@
 import { processFontFamily } from "expo-font";
 import { Text, View, StyleSheet } from "react-native";
+import { OPENSANS_REGULAR } from "../../utils/const";
 
 const styles = StyleSheet.create({
   review: {
     fontSize: 30,
-    fontFamily: "OpenSans-Regular",
+    fontFamily: OPENSANS_REGULAR,
   },
 });
 const DetailScreen = () => {
