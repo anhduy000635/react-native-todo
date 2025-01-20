de01ab67899b8592fbe4acc4ec04c625cfcb1c63
anhduy00635 <anhduy000635@gmail.com>
2025-01-20 14:43:35 +0700

    Icons

--- App.tsx
+++ App.tsx
@@ -14,90 +14,94 @@ import {
   TextInput,
   View,
 } from "react-native";
-import FlexBox from "./components/flex.box";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 export default function App() {
-  // const [todo, setTodo] = useState("");
-  // const [listTodo, setListTodo] = useState<ITodo[]>([]);
-  // interface ITodo {
-  //   id: number;
-  //   name: string;
-  // }
-  // function randomTodo(min: number, max: number) {
-  //   return Math.floor(Math.random() * (max - min + 1) + min);
-  // }
-  // const handleAddTodo = () => {
-  //   if (!todo) {
-  //     Alert.alert("Lỗi Input để trống", "Vui lồn nhập", [
-  //       {
-  //         text: "Cancel",
-  //         onPress: () => console.log("Cancel Pressed"),
-  //         style: "cancel",
-  //       },
-  //       { text: "OK", onPress: () => console.log("OK Pressed") },
-  //     ]);
-  //   } else {
-  //     setListTodo([...listTodo, { id: randomTodo(1, 100), name: todo }]);
-  //     setTodo("");
-  //   }
-  // };
+  const [todo, setTodo] = useState("");
+  const [listTodo, setListTodo] = useState<ITodo[]>([]);
+  interface ITodo {
+    id: number;
+    name: string;
+  }
+  function randomTodo(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  const handleAddTodo = () => {
+    if (!todo) {
+      Alert.alert("Lỗi Input để trống", "Vui lồn nhập", [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
+        },
+        { text: "OK", onPress: () => console.log("OK Pressed") },
+      ]);
+    } else {
+      setListTodo([...listTodo, { id: randomTodo(1, 100), name: todo }]);
+      setTodo("");
+    }
+  };
   // const handleRemoveTodo = (id: number) => {
   //   const newTodo = listTodo.filter((item) => item.id !== id);
   //   setListTodo(newTodo);
   // };
   return (
-    <FlexBox />
-    // <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-    //   <View style={styles.container}>
-    //     {/* Header */}
-    //     <Text style={styles.header}>Todo APP</Text>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        {/* Header */}
+        <Text style={styles.header}>Todo APP</Text>
 
-    //     {/* Form */}
-    //     <View style={styles.body}>
-    //       <TextInput
-    //         value={todo}
-    //         style={styles.todoInput}
-    //         onChangeText={(value) => setTodo(value)}
-    //       />
-    //       <Button title="Add" onPress={handleAddTodo} />
-    //     </View>
-    //     {/* List Todo */}
-    //     <View style={styles.body}>
-    //       <FlatList
-    //         data={listTodo}
-    //         keyExtractor={(item) => item.id.toString()}
-    //         renderItem={({ item }) => {
-    //           return (
-    //             <Pressable
-    //               style={({ pressed }) => ({ opacity: pressed ? 0.5 : 1 })}
-    //               onPress={() => handleRemoveTodo(item.id)}
-    //             >
-    //               <Text style={styles.todoItem}>{item.name}</Text>
-    //             </Pressable>
-    //           );
-    //         }}
-    //       />
-    //     </View>
-    //   </View>
-    // </TouchableWithoutFeedback>
+        {/* Form */}
+        <View style={styles.form}>
+          <TextInput
+            value={todo}
+            style={styles.todoInput}
+            onChangeText={(value) => setTodo(value)}
+          />
+          <Button title="Add" onPress={handleAddTodo} />
+        </View>
+        {/* List Todo */}
+        <View style={styles.todo}>
+          <FlatList
+            data={listTodo}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => {
+              return (
+                <Pressable
+                  style={({ pressed }) => ({ opacity: pressed ? 0.5 : 1 })}
+                  // onPress={() => handleRemoveTodo(item.id)}
+                >
+                  <View style={styles.groupTodo}>
+                    <Text style={styles.todoItem}>{item.name}</Text>
+                    <AntDesign name="delete" size={24} color="black" />
+                  </View>
+                </Pressable>
+              );
+            }}
+          />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40,
+    paddingBottom: 40,
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
   header: {
     backgroundColor: "orange",
     paddingHorizontal: 20,
     textAlign: "center",
     paddingVertical: 10,
-    fontSize: 30,
-  },
-  container: {
+    fontSize: 60,
     flex: 1,
-    paddingTop: 40,
-    paddingHorizontal: 20,
-    backgroundColor: "#fff",
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
   todoInput: {
     height: 40,
@@ -108,15 +112,24 @@ const styles = StyleSheet.create({
     marginHorizontal: 30,
     margin: 15,
   },
-  body: {
-    paddingHorizontal: 10,
-    marginBottom: 20,
+  groupTodo: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "green",
+    marginBottom: 15,
+    justifyContent: "space-between",
+    padding: 15,
+    marginHorizontal: 10,
   },
   todoItem: {
     fontSize: 20,
-    marginBottom: 25,
-    borderWidth: 1,
-    borderColor: "green",
-    padding: 10,
+  },
+  form: {
+    flex: 2,
+    marginBottom: 20,
+  },
+  todo: {
+    flex: 7,
   },
 });
